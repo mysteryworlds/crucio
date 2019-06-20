e5f5902dd801d23589ba0f1ce06df0fc7f693487
Felix Klauke <info@felix-klauke.de>
2019-06-21 00:20:44 +0200

    Add spigot and adjust other versions.

--- spigot/src/main/java/com/mysteryworlds/crucio/spigot/CrucioSpigotPlugin.java
+++ spigot/src/main/java/com/mysteryworlds/crucio/spigot/CrucioSpigotPlugin.java
@@ -1,7 +1,29 @@
 package com.mysteryworlds.crucio.spigot;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import com.google.inject.Binder;
+import com.mysteryworlds.crucio.spigot.module.CrucioSpigotModule;
+import de.d3adspace.isabelle.spgiot.theresa.IsabelleSpigotExtension;
+import de.d3adspace.theresa.lifecycle.annotation.WarmUp;
 
-public class CrucioSpigotPlugin extends JavaPlugin {
+import javax.annotation.PreDestroy;
 
+public class CrucioSpigotPlugin extends IsabelleSpigotExtension {
+
+    @WarmUp
+    public void onStart() {
+
+
+    }
+
+    @PreDestroy
+    public void onStop() {
+
+
+    }
+
+    @Override
+    public void configure(Binder binder) {
+
+        binder.install(new CrucioSpigotModule());
+    }
 }
---
+++ spigot/src/main/java/com/mysteryworlds/crucio/spigot/module/CrucioSpigotModule.java
@@ -0,0 +1,12 @@
+package com.mysteryworlds.crucio.spigot.module;
+
+import com.google.inject.AbstractModule;
+
+public class CrucioSpigotModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+
+
+    }
+}
