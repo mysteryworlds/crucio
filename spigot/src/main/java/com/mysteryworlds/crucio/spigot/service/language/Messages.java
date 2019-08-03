1fce4a89dd9e2672da7d4a8917adf4e746d65671
Felix Klauke <info@felix-klauke.de>
2019-08-03 20:27:43 +0200

    Use correct message key for quit message.

--- spigot/src/main/java/com/mysteryworlds/crucio/spigot/service/language/Messages.java
+++ spigot/src/main/java/com/mysteryworlds/crucio/spigot/service/language/Messages.java
@@ -4,6 +4,6 @@ public class Messages {
 
   public static final String KEY_MESSAGE_PREFIX = "prefix";
   public static final String KEY_MESSAGE_JOIN = "join";
-  public static final String KEY_MESSAGE_QUIT = ".quit";
+  public static final String KEY_MESSAGE_QUIT = "quit";
   public static final String KEY_MESSAGE_KICK_LEAVE = "kick-leave";
 }
