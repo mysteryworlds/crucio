9722ccccfd22846ee2f668be4f084649c6a8774f
Felix Klauke <info@felix-klauke.de>
2020-03-26 17:54:22 +0100

    Remove legacy sources

--- crucio-core/src/main/java/com/mysteryworlds/crucio/core/CrucioCore.java
+++
@@ -1,5 +0,0 @@
-package com.mysteryworlds.crucio.core;
-
-public class CrucioCore {
-
-}
--- crucio-spigot/src/main/java/com/mysteryworlds/crucio/spigot/service/language/Messages.java
+++
@@ -1,9 +0,0 @@
-package com.mysteryworlds.crucio.spigot.service.language;
-
-public class Messages {
-
-  public static final String KEY_MESSAGE_PREFIX = "prefix";
-  public static final String KEY_MESSAGE_JOIN = "join";
-  public static final String KEY_MESSAGE_QUIT = "quit";
-  public static final String KEY_MESSAGE_KICK_LEAVE = "kick-leave";
-}
